71ac141eb2d25fc8f77f34202defc5b11d352c11
Colin Bernet <colin.bernet@cern.ch>
2019-02-21 16:23:39 +0100

    yelp preprocessing done

--- yelp/yelp_preprocessing.py
+++ yelp/yelp_preprocessing.py
@@ -1,3 +1,30 @@
+import json
 import pandas as pd
 
-pd.read_json('/Users/cbernet/Datasets/MachineLearning/yelp_dataset/review.json')
+# open input file: 
+fname = 'review.json'
+ifile = open(fname) 
+
+# read the first 100k entries
+# set to -1 to process everything
+stop = -1
+
+all_data = list()
+for i, line in enumerate(ifile):
+    # convert the json on this line to a dict
+    data = json.loads(line)
+    # extract what we want
+    text = data['text']
+    stars = data['stars']
+    # add to the data collected so far
+    all_data.append([stars, text])
+    if i%10000==0:
+        print(i)
+    if i==stop:
+        break
+# create the DataFrame
+df = pd.DataFrame(all_data, columns=['stars','text'])
+print(df)
+
+ifile.close()
+
