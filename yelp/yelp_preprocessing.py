fdc6221443103cc7060d24e8aea6be53e07f129e
Colin Bernet <colin.bernet@cern.ch>
2019-02-27 05:56:49 +0100

    fix basic script

--- yelp/yelp_preprocessing.py
+++ yelp/yelp_preprocessing.py
@@ -1,18 +1,20 @@
 import json
-import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # open input file: 
-fname = sys.argv[1]
-ifile = open(fname) 
+ifile = open('review.json') 
 
 # read the first 100k entries
 # set to -1 to process everything
-stop = 10000
+stop = -1
 
 all_data = list()
 for i, line in enumerate(ifile):
+    if i%10000==0:
+        print(i)
+    if i==stop:
+        break    
     # convert the json on this line to a dict
     data = json.loads(line)
     # extract what we want
@@ -20,10 +22,6 @@ for i, line in enumerate(ifile):
     stars = data['stars']
     # add to the data collected so far
     all_data.append([stars, text])
-    if i%10000==0:
-        print(i)
-    if i==stop:
-        break
 # create the DataFrame
 df = pd.DataFrame(all_data, columns=['stars','text'])
 print(df)
