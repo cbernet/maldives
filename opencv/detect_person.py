c3ec454df5e81a3a79949163b4697e16586eba24
Colin <colin.bernet@cern.ch>
2019-04-25 13:07:51 +0200

    started benchmarking

--- opencv/detect_person.py
+++ opencv/detect_person.py
@@ -1,54 +1,78 @@
 # import the necessary packages
 import numpy as np
 import cv2
- 
+from framecounter import FrameCounter
+
+do_write = False
+do_display = False
+do_detection = True
+# open webcam video stream
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('walking.mp4')
+
 # initialize the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 cv2.startWindowThread()
 
-# open webcam video stream
-cap = cv2.VideoCapture(0)
+if do_write:
+    # the output will be written to output.avi
+    out = cv2.VideoWriter(
+        'output.avi',
+        cv2.VideoWriter_fourcc(*'MJPG'),
+        15.,
+        (640,480))
 
-# the output will be written to output.avi
-out = cv2.VideoWriter(
-    'output.avi',
-    cv2.VideoWriter_fourcc(*'MJPG'),
-    15.,
-    (640,480))
+fcount = FrameCounter()
 
+i=0
 while(True):
+    i+=1
     # Capture frame-by-frame
     ret, frame = cap.read()
-
-    # resizing for faster detection
-    frame = cv2.resize(frame, (640, 480))
-    # using a greyscale picture, also for faster detection
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-
-    # detect people in the image
-    # returns the bounding boxes for the detected objects
-    boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
-
-    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
-
-    for (xA, yA, xB, yB) in boxes:
-        # display the detected boxes in the colour picture
-        cv2.rectangle(frame, (xA, yA), (xB, yB),
-                          (0, 255, 0), 2)
-    
-    # Write the output video 
-    out.write(frame.astype('uint8'))
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if ret is False:
         break
 
+    fcount.start()
+
+    if do_detection:
+        # resizing for faster detection
+        # frame = cv2.resize(frame, (640, 480))
+        # using a greyscale picture, also for faster detection
+        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+        # detect people in the image
+        # returns the bounding boxes for the detected objects
+        boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
+        
+        boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+        
+        for (xA, yA, xB, yB) in boxes:
+            # display the detected boxes in the colour picture
+            cv2.rectangle(frame, (xA, yA), (xB, yB),
+                          (0, 255, 0), 2)
+
+    if do_write:
+        # Write the output video 
+        out.write(frame.astype('uint8'))
+        
+    if do_display:
+        # Display the resulting frame
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    fcount.stop()
+    
+    if i%10 == 0:
+        print(fcount)
+    
 # When everything done, release the capture
 cap.release()
 # and release the output
-out.release()
+if do_write: 
+    out.release()
 # finally, close the window
 cv2.destroyAllWindows()
 cv2.waitKey(1)
--- opencv/video_in.py
+++ opencv/video_in.py
@@ -1,23 +1,35 @@
 import numpy as np
 import cv2
+import time
+from framecounter import FrameCounter
+
+fcount = FrameCounter()
 
 cv2.startWindowThread()
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('walking.mp4')
 
+i = 0
 while(True):
+    i+=1
     ret, frame = cap.read()
+
+    fcount.start()
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     ret,frame = cv2.threshold(frame,80,255,cv2.THRESH_BINARY)
-
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        # breaking the loop if the user types q
-        # note that the video window must be highlighted!
-        break
+    
+    # cv2.imshow('frame',frame)
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #    # breaking the loop if the user types q
+    #    # note that the video window must be highlighted!
+    #    break
+    fcount.stop()
+    
+    if i%10==0:
+        print(fcount)
 
 cap.release()
-cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
 # the following is necessary on the mac,
 # maybe not on other platforms:
-cv2.waitKey(1)
+# cv2.waitKey(1)
 
