4482522e84061b4148ada08c25af48c59290f80d
Colin Bernet <colin.bernet@cern.ch>
2019-06-06 12:29:34 +0200

    a few updates

--- bokeh/bokeh_power_update.py
+++ bokeh/bokeh_power_update.py
@@ -1,12 +1,13 @@
 from bokeh.layouts import column
 from bokeh.plotting import figure, curdoc
-from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.models import ColumnDataSource, HoverTool, Range1d, Label
 
 import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
 
 import pymongo
+import math
 
 def preprocess_df(df):
     del df['_id']
@@ -49,14 +50,39 @@ p1.xaxis.axis_label='Time'
 p1.yaxis.axis_label='Power (kW)'
 
 # gauges 
-channels = ['Main', 'Pool', 'Heater']
-gauge_source = [df.tail(1)['power'], 0., 0.]
-print(gauge_source)
-p2 = figure(x_range=channels, plot_height=250, title='Current power')
-p2.vbar(top=gauge_source,
-        x=channels,
-        width=0.9)
-p2.yaxis.axis_label='Power (kW)'
+# channels = ['Main', 'Pool', 'Heater']
+# gauge_source = [df.tail(1)['power'], 0., 0.]
+# print(gauge_source)
+# p2 = figure(x_range=channels, plot_height=250, title='Current power')
+# p2.vbar(top=gauge_source,
+#         x=channels,
+#         width=0.9)
+# p2.yaxis.axis_label='Power (kW)'
+
+p = figure(width=400, height=200, tools=[])
+
+maxv = 10
+def angle(x):
+    x = min(x,maxv)
+    return (1-x/maxv)*math.pi
+value = 8.1
+sa = angle(value)
+
+ds = ColumnDataSource(dict(sa=[sa],valtext=['{:3.2f}'.format(value)]))
+p.annular_wedge(x=0, y=0, inner_radius=0.55, outer_radius=0.8,
+                start_angle='sa', end_angle=math.pi, color="red", alpha=0.6, source = ds)
+
+# cosmetics
+p.y_range=Range1d(-0.1,1)
+p.toolbar_location=None
+p.axis.visible = False
+p.grid.visible = False
+mytext = Label(x=0, y=0, 
+               text='{:3.2f}'.format(value), 
+               text_font_size='50pt', text_align='center')
+mytext2 = Label(x=0, y=-0.07, text='Main', text_font_size='16pt', text_align='center')
+p.add_layout(mytext)
+p.add_layout(mytext2)
 
 def update():
     global last_id
@@ -68,7 +94,11 @@ def update():
         df = pd.DataFrame(data)
         preprocess_df(df)
         source.stream(df)
+        last_val = df['power'].values[-1]
+        mytext.text = '{:3.2f}'.format(last_val)
+        ds.patch({'sa':[(0,angle(last_val))]})
 
-layout = column(p2)
-curdoc().add_root(p1)
+
+layout = column([p1,p])
+curdoc().add_root(layout)
 curdoc().add_periodic_callback(update, 1000)
